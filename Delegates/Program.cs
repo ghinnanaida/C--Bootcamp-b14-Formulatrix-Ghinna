998a09e389e60df1cd8f1f3f8115c67518fe29c0
ghinnanaida <ghinnanaida08@gmail.com>
2025-07-18 15:00:58 +0700

    Add latihan day 5

---
+++ Delegates/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+delegate int OperasiMatematika(int a, int b);
+
+class Program
+{
+    
+    
+    static void Main()
+    {
+        CobaDelegates();
+    }
+
+    #region Delegates
+
+    static int Tambah (int a, int b) => a + b;
+    static int Kurang (int a, int b) => a - b;
+    static int Kali (int a, int b) => a * b;
+    static int Bagi (int a, int b) => a / b;
+
+    static void JalankanOperasi(OperasiMatematika operasi, int a, int b)
+    {
+        Console.WriteLine($"{operasi.Method.Name} {a} & {b}\nHasil = {operasi(a, b)}");
+    }
+
+    static void JalankanOperasiDuaKali(OperasiMatematika operasi, int a, int b)
+    {
+        operasi += operasi;
+
+        var hasil = operasi.GetInvocationList();
+        int hasil1 = (int)hasil[0].DynamicInvoke(a,b);
+        int hasil2 = (int)hasil[1].DynamicInvoke(a,b);
+        Console.WriteLine($"{hasil[0].Method.Name} {a} & {b}\nHasil pertama = {hasil1}\n{hasil[1].Method.Name} {a} & {b}\nHasil kedua = {hasil2}");
+    }
+    public class Operasi{
+        public int Tambahkan (int a, int b) => a + b;
+        public int Kurangkan (int a, int b) => a - b;
+        public static int Kalikan (int a, int b) => a * b;
+        public static int Bagikan (int a, int b) => a / b;
+    }
+    static void CobaDelegates()
+    {
+        JalankanOperasi(Tambah, 10, 5);
+        JalankanOperasiDuaKali(Tambah, 10, 5);
+        JalankanOperasi(Kurang, 10, 5);
+        JalankanOperasi(Kali, 10, 5);
+        JalankanOperasi(Bagi, 10, 5);
+
+        Operasi operasi = new Operasi();
+        JalankanOperasi(operasi.Tambahkan, 10, 5);
+        JalankanOperasi(operasi.Kurangkan, 10, 5);
+        JalankanOperasi(Operasi.Kalikan, 10, 5);
+        JalankanOperasi(Operasi.Bagikan, 10, 5);
+    }
+    #endregion
+
+
+    #region Event Handler
+    #endregion
+}
---
+++ Event/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        var supermarket = new Supermarket();
+        var pelanggan1 = new Pelanggan("Ghinna");
+        var pelanggan2 = new Pelanggan("Naida");
+
+        supermarket.Promo += pelanggan1.TerimaPromo;
+        supermarket.Promo += pelanggan2.TerimaPromo;
+
+        supermarket.UmumkanPromo("1 Kg Mangga", 20000);
+
+        // supermarket.Promo -= pelanggan2.TerimaPromo;
+        // supermarket.UmumkanPromo("1 Kg Apel", 25000);
+    }
+
+    public delegate void PromoEventHandler(object? sender, string promo, int harga);
+
+    public class Supermarket
+    {
+        public event PromoEventHandler Promo;
+
+        public void UmumkanPromo(string promo, int harga)
+        {
+            Console.WriteLine($"📢 Supermarket sedang promo: {promo} menjadi harga Rp {harga}! Dapatkan segera !!");
+            Promo?.Invoke(promo, harga);
+        }
+    }
+
+    public class Pelanggan
+    {
+        public string namaPelanggan { get; }
+        public Pelanggan(string nama)
+        {
+            namaPelanggan=nama;
+        }
+
+        public void TerimaPromo(object? supermarket, string promo, int harga)
+        {
+            Console.WriteLine($"👤 {namaPelanggan} menerima promo: {promo}");
+        }
+    }
+}
