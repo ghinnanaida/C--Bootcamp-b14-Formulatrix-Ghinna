ea4671410dd841ced330774282a28d29954703d5
ghinnanaida <ghinna.naida@dattabot.io>
2025-07-22 00:02:56 +0700

    Update Foobar logic rule

--- Project1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
+++ Project1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86c80c386c200bee734f224fa46a48c3a945edc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8237e16029610ff7fdbb92dc5ceed8575f3a2e73")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
