f57e99dcab5afaa0d9df71c8442caeb80515b50c
ghinnanaida <ghinnanaida08@gmail.com>
2025-07-28 10:53:23 +0700

    Update Foobar logic

--- FooBar/Program.cs
+++ FooBar/Program.cs
@@ -6,99 +6,129 @@ class Program
 
     static void Main()
     {
-        FooBarJazzLooping1();
-        FooBarJazzLooping2();
-        FooBarJazzUseDelegates();
+        int number = 40;
+        FooBarJazzLooping1(number);
+        FooBarJazzLooping2(number);
+        FooBarJazzUseDelegates(number);
+        FooBarJazzUseDictionary(number);
     }
 
-    delegate void Rules(int num, StringBuilder sb);
-
-    static void FooBarJazzUseDelegates()
+    static void FooBarJazzUseDictionary(int number)
     {
-        Console.WriteLine("\nFooBarJazz using delegates implementation..");
-        int num = 35;
+        Console.WriteLine("\n\nFooBarJazz use dictionary looping for rules..");
+            
+        Dictionary<int, string> ruleMap = new Dictionary<int, string>
+        {
+            {3,"foo"},
+            {4,"baz"},
+            {5,"bar"},
+            {7,"jazz"},
+            {9,"huzz"},
+        };
+        
+        void Rules(int num, int denominator, string word, StringBuilder sb) => sb.Append(num % denominator == 0 ? word : "");
 
-        void Rules1(int num, StringBuilder sb) => sb.Append(num % 3 == 0 ? "foo" : "");
-        void Rules2(int num, StringBuilder sb) => sb.Append(num % 4 == 0 ? "baz" : "");
-        void Rules3(int num, StringBuilder sb) => sb.Append(num % 5 == 0 ? "bar" : "");
-        void Rules4(int num, StringBuilder sb) => sb.Append(num % 7 == 0 ? "jazz" : "");
-        void Rules5(int num, StringBuilder sb) => sb.Append(num % 9 == 0 ? "huzz" : "");
-
-        Rules r = Rules1;
-        r += Rules2;
-        r += Rules3;
-        r += Rules4;
-        r += Rules5;
-
-        for (int i = 1; i <= num; i++)
+        for (int num = 1; num <= number; num++)
         {
             StringBuilder result = new StringBuilder();
 
-            r(i, result);
+            foreach (var rule in ruleMap)
+            {
+                int denominator = rule.Key;
+                string word = rule.Value;
+
+                Rules(num, denominator, word, result);
+            }
 
             if (result.Length == 0)
-                result.Append(i);
+                result.Append(num);
 
             Console.Write(result);
 
-            if (i != num)
+            if (num != number)
+                Console.Write(", ");
+        }
+
+    }
+    delegate void Rules(int num, StringBuilder sb);
+
+    static void FooBarJazzUseDelegates(int number)
+    {
+        Console.WriteLine("\n\nFooBarJazz using delegates implementation..");
+
+
+        void Rules1(int number, StringBuilder sb) => sb.Append(number % 3 == 0 ? "foo" : "");
+        void Rules2(int number, StringBuilder sb) => sb.Append(number % 4 == 0 ? "baz" : "");
+        void Rules3(int number, StringBuilder sb) => sb.Append(number % 5 == 0 ? "bar" : "");
+        void Rules4(int number, StringBuilder sb) => sb.Append(number % 7 == 0 ? "jazz" : "");
+        void Rules5(int number, StringBuilder sb) => sb.Append(number % 9 == 0 ? "huzz" : "");
+
+        Action<int, StringBuilder> ruleChain = Rules1;
+        ruleChain += Rules2;
+        ruleChain += Rules3;
+        ruleChain += Rules4;
+        ruleChain += Rules5;
+
+        for (int num = 1; num <= number; num++)
+        {
+            StringBuilder result = new StringBuilder();
+
+            ruleChain(num, result);
+
+            if (result.Length == 0)
+                result.Append(num);
+
+            Console.Write(result);
+
+            if (num != number)
                 Console.Write(", ");
         }
     }
 
-    static void FooBarJazzLooping1()
+    static void FooBarJazzLooping1(int number)
     {
-        Console.WriteLine("\nFooBarJazz only looping with StringBuilder Append..");
+        Console.WriteLine("\n\nFooBarJazz only looping with StringBuilder Append..");
 
-        int num = 35;
-
-        for (int i = 1; i <= num; i++)
+        for (int num = 1; num <= number; num++)
         {
             StringBuilder result = new StringBuilder();
             
-            if (i % 3 == 0) result.Append("foo");
-            if (i % 4 == 0) result.Append("baz");
-            if (i % 5 == 0) result.Append("bar");
-            if (i % 7 == 0) result.Append("jazz");
-            if (i % 9 == 0) result.Append("huzz");
-            if (result.Length == 0) result.Append(i);
+            if (num % 3 == 0) result.Append("foo");
+            if (num % 4 == 0) result.Append("baz");
+            if (num % 5 == 0) result.Append("bar");
+            if (num % 7 == 0) result.Append("jazz");
+            if (num % 9 == 0) result.Append("huzz");
+            if (result.Length == 0) result.Append(num);
                
             Console.Write(result);
 
-            if (i != num)
+            if (num != number)
                 Console.Write(", ");
         }
     }
 
-    static void FooBarJazzLooping2()
+    static void FooBarJazzLooping2(int number)
     {
-        Console.WriteLine("\nFooBarJazz only looping through if conditional..");
+        Console.WriteLine("\n\nFooBarJazz only looping through if conditional..");
 
-        int num = 35;
-
-        for (int i = 1; i <= num; i++)
+        for (int num = 1; num <= number; num++)
         {
-            if (i % 3 != 0 && i % 4 != 0 && i % 5 != 0 && i % 7 != 0 && i % 9 != 0)
-                Console.Write(i);
-            if (i % 3 == 0)
+            if (num % 3 != 0 && num % 4 != 0 && num % 5 != 0 && num % 7 != 0 && num % 9 != 0)
+                Console.Write(num);
+            if (num % 3 == 0)
                 Console.Write("foo");
-            if (i % 4 == 0)
+            if (num % 4 == 0)
                 Console.Write("baz");
-            if (i % 5 == 0)
+            if (num % 5 == 0)
                 Console.Write("bar");
-            if (i % 7 == 0)
+            if (num % 7 == 0)
                 Console.Write("jazz");
-            if (i % 9 == 0)
+            if (num % 9 == 0)
                 Console.Write("huzz");
-            if (i != num)
+            if (num != number)
                 Console.Write(", ");
         }
-    }
-
-    static void FooBarJazzUseListGenerics()
-    {
-        throw new NotImplementedException("ntar implementnya, dah kebayang tapi masih bingung nulisnya");
-    }
+    }  
 }
 
 
