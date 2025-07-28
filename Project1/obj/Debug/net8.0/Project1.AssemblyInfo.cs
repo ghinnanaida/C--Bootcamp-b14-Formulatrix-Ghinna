34800178a054761175c2123dc475fdda6fa1e476
ghinnanaida <ghinnanaida08@gmail.com>
2025-07-28 13:14:50 +0700

    Add new chess project

--- Project1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
+++ Project1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ee41e1b89a9881b8493b38616074d6505a591ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32b57c269b0b431cac857e0395913e29d689e8d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
