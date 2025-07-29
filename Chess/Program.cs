1123afe2966f5e594f4ba21b40bfdd027f4499b9
ghinnanaida <ghinnanaida08@gmail.com>
2025-07-29 13:39:56 +0700

    Update template each class and game control, make sub folder for classes, enums, interfaces, records, and controller

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
+namespace ChessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            
+        }
+    }
+}
