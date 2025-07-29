cd9a7198c5ec3701591166b661710fd36b3ede94
ghinnanaida <ghinnanaida08@gmail.com>
2025-07-29 13:38:26 +0700

    Update template each class and game control, make sub folder for classes, enums, interfaces, records, and controller

--- Project1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
+++ Project1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9695394eee6edd26e8373a0e29c73553f2ab6a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+925ae0b45ca501c0f9235aa4ad67ca6ef8675ca4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
