785c61ceec5bc77945719f05370cd01a74736a02
ghinnanaida <ghinna.naida@dattabot.io>
2025-08-05 23:13:34 +0700

    Update sln

--- Project1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
+++ Project1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+764993c46338a656bb63b6c2ecaac75aae00605e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7df613746ab47a9ce0e2364b1925fed3f0ca7dd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
